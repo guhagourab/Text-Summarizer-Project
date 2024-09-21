b51f8aa0bca1a65ff69eea1082294941ff1894aa
gourabguha007 <gourab.g@epneo.com>
2024-09-21 22:24:26 +0530

    requirements and installations done

--- template.py
+++ template.py
@@ -2,22 +2,23 @@ import os
 from pathlib import Path
 import logging
 
-logging.basicConfig(level=logging.INFO, format='[%(asctime)s]:%(message)s')
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
 
-project_name ="textsummarizer"
+
+project_name = "textSummarizer"
 
 list_of_files = [
     ".github/workflows/.gitkeep",
-    f"src/{project_name}/_init_.py",
-    f"src/{project_name}/components/_init_.py",
-    f"src/{project_name}/utils/_init_.py",
+    f"src/{project_name}/__init__.py",
+    f"src/{project_name}/conponents/__init__.py",
+    f"src/{project_name}/utils/__init__.py",
     f"src/{project_name}/utils/common.py",
-    f"src/{project_name}/logging/_init_.py",
-    f"src/{project_name}/config/_init_.py",
+    f"src/{project_name}/logging/__init__.py",
+    f"src/{project_name}/config/__init__.py",
     f"src/{project_name}/config/configuration.py",
-    f"src/{project_name}/pipeline/_init_.py",
-    f"src/{project_name}/entity/_init_.py",
-    f"src/{project_name}/constants/_init_.py",
+    f"src/{project_name}/pipeline/__init__.py",
+    f"src/{project_name}/entity/__init__.py",
+    f"src/{project_name}/constants/__init__.py",
     "config/config.yaml",
     "params.yaml",
     "app.py",
@@ -25,9 +26,10 @@ list_of_files = [
     "Dockerfile",
     "requirements.txt",
     "setup.py",
-    "research/trials.ipynb"
+    "research/trials.ipynb",
 
 ]
+
 
 for filepath in list_of_files:
     filepath = Path(filepath)
